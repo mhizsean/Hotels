fd27f2cae2cebee0d7029bcb59057766ec604d96
mhizsean <adeyiesther01@gmail.com>
2020-03-11 12:21:52 +0100

    first commit

---
+++ src/pages/Rooms.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Hero from '../components/Hero';
+
+export const Rooms = () => {
+    return (
+        <Hero hero="roomsHero" />
+           
+    )
+};
+
+export default Rooms;
