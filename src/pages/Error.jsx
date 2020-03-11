fe60142a2e43234385002c2c5fe80ab7b56cabe4
mhizsean <adeyiesther01@gmail.com>
2020-03-11 13:15:01 +0100

    error page, banner

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
 import Hero from '../components/Hero';
+import { Link } from 'react-router-dom';
+import Banner from '../components/Banner';
 
 export default function Error() {
     return (
-        <Hero />
+        <Hero>
+            <Banner title="404" subtitle="page not found">
+                <Link to="/" className="btn-primary">
+                    Return home
+                </Link>
+            </Banner>
+        </Hero>
+        
     )
 }
